7a31f64d365a52ff30fff2618967319da1ff24ef
Pary <81phary@gmail.com>
2022-04-06 15:23:17 +1000

    innitial setup

---
+++ MvcE-Commerce/Infrastructure/CmsShoppingCartContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcE_Commerce.Infrastructure
+{
+    public class CmsShoppingCartContext : DbContext
+
+    {
+        public CmsShoppingCartContext(DbContextOptions<CmsShoppingCartContext> options) : base(options)
+        {
+            
+        }
+    }
+}
